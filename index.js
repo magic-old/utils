623927655bc24eb6c5f325b80c77fa966cc05a8d
Jascha Ehrenreich <jascha@jaeh.at>
2015-09-02 19:49:24 +0200

    add isNumber

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ export function isType(type, node, key = false) {
     return typeof node === type;
   }
 
-  return node[key] && typeof node[key] === type;
+  return node && node[key] && typeof node[key] === type;
 }
 
 export function isFunction(node, key = false) {
   return isType('function', node, key);
+}
+
+export function isNumber(val, key = false) {
+  if (!key) {
+    return isType('number', val) && parseInt(val) === parseInt(val);
+  }
+
+  return isType('number', val, key) && parseInt(val[key]) === parseInt(val[key]);
 }
