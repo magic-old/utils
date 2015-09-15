f43aaa2eb9c61b99620b0db8d22b7b93af7b1795
Jascha Ehrenreich <jascha@jaeh.at>
2015-09-15 21:51:50 +0200

    modularized router

--- router.js
+++ router.js
@@ -1,44 +1,46 @@
-import {isNumber, hasClass} from './index';
+import {isNumber, isFunction, isString, hasClass} from './index';
 import page from 'page';
 
-page('*', show);
-page('*', show404);
-page();
-
 const header = document.querySelector('header.main');
-const nav = document.querySelector('nav.main');
 const body = document.body;
 
-const allLinks = document.querySelectorAll('a');
+const defaultRoutes = [
+  {path: '*', handler: show},
+  {path: '*', handler: show404},
+];
 
-Object.keys(allLinks).forEach(key => {
-  const link = allLinks[key];
-  if (link && link.href) {
-    const linkArray = link.href.split('#');
-    if (linkArray.length > 1) {
-      const href = linkArray[linkArray.length - 1];
-      link.href = `/${href}`;
+export default function route(routes = defaultRoutes) {
+  const {pathname} = window.location;
+  const allLinks = document.querySelectorAll('a');
+
+  Object.keys(allLinks).forEach(key => {
+    const link = allLinks[key];
+    if (link && link.href) {
+      const linkArray = link.href.split('#');
+      if (linkArray.length > 1) {
+        const href = linkArray[linkArray.length - 1];
+        link.href = `/${href}`;
+      }
     }
+  });
+
+  if (isFunction(routes, 'forEach')) {
+    routes.forEach((r) => {
+      const {path, handler} = r;
+      if (isString(path) && isFunction(handler)) {
+        page(path, handler);
+      }
+    });
   }
-});
+  page();
+}
 
 function show(ctx, next) {
   let selector = (ctx.pathname === '/')
                  ? '#♥'
                  : ctx.pathname.replace('/', '#');
 
-  const target = document.querySelector(selector);
-
-  if (!target) {
-    return next();
-  }
-
-  let {offsetTop} = target;
-  if (hasClass(body, 'scrolled')) {
-    if (nav && nav.clientHeight) {
-      offsetTop -= nav.clientHeight;
-    }
-  }
+  const offsetTop = getTopOffset(selector, next);
 
   if (isNumber(offsetTop)) {
     window.scrollTo(0, offsetTop);
@@ -49,4 +51,32 @@ function show(ctx, next) {
 
 function show404() {
   console.log('404');
+}
+
+function getTopOffset(selector, next) {
+  const target = document.querySelector(selector);
+
+  if (!target || !header) {
+    return next();
+  }
+
+  let {offsetTop} = target;
+
+  const {clientHeight: heightBefore} = header;
+
+  if (!hasClass(body, 'scrolled')) {
+    if (window.scrollY > 40) {
+      body.classList.add('scrolled');
+    }
+
+    const {clientHeight: heightAfter} = header;
+    const totalOffset = heightBefore 
+                        ? heightBefore - heightAfter
+                        : heightAfter;
+    if (totalOffset) {
+      // offsetTop -= totalOffset;
+    }
+  }
+
+  return offsetTop;
 }
